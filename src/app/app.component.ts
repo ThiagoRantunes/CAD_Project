785ac0e71a855254c46fd52ea56a5afbe1789068
ThiagoRantunes <thiagorantunes05@gmail.com>
2024-08-27 16:22:31 -0300

    progress-4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { ControlerPageComponent } from './controler-page/controler-page.component';
 import { ListPageComponent } from './list-page/list-page.component';
+import { ModalComponent } from './modal/modal.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavBarComponent, ControlerPageComponent, ListPageComponent],
+  imports: [RouterOutlet, NavBarComponent, ControlerPageComponent, ListPageComponent, ModalComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
