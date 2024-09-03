b7b3e964690aaf15a4afdfba33cb8bff8402def3
ThiagoRantunes <thiagorantunes05@gmail.com>
2024-09-03 14:05:46 -0300

    insert-navbar-calculadoda-page

--- src/app/calculadora-page/calculadora-page.component.ts
+++ src/app/calculadora-page/calculadora-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NavBarComponent } from '../nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-calculadora-page',
   standalone: true,
-  imports: [],
+  imports: [NavBarComponent],
   templateUrl: './calculadora-page.component.html',
   styleUrl: './calculadora-page.component.scss'
 })
